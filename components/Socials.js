9e0bedc7d820f8b6b20584b400eea71cfb951a92
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-08 23:36:07 +0800

    Add font

--- components/Socials.js
+++ components/Socials.js
@@ -8,11 +8,29 @@ import styles from "../styles/Socials.module.css"
 
 export default function Socials() {
     return (
-        <section className={styles.socials}>
-            <FontAwesomeIcon icon={faHandSpock} />
-            <FontAwesomeIcon icon={faTwitter} />
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faFacebook} />
+        <section className={styles.socialsSection}>
+            <div className={styles.socials}>
+                <div className={styles.socialHeader}>
+                    <p>Artist / Illustrator</p>
+                    <p>Hicami</p>
+                    <p>Something else</p>
+                </div>
+                <div className={styles.socialContent}>
+                    <p>Brush strokes on canvas,<br></br>Creations of my heart and soul,<br></br>For sale, now behold.</p>
+                    <a href="mailto:xxx@gmail.com">Hire me!</a>
+                </div>
+                <div className={styles.socialFooter}>
+                    <p>Artist / Illustrator</p>
+                    <div className={styles.socialIcons}>
+                        <FontAwesomeIcon icon={faHandSpock} />
+                        <FontAwesomeIcon icon={faTwitter} />
+                        <FontAwesomeIcon icon={faInstagram} />
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </div>
+                    <p>Something else</p>
+                </div>
+
+            </div>
         </section>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 // `pages/_app.js`
 import { AppProvider } from "../context/AppContext.js";
-import '../styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core'
+import { Quicksand } from '@next/font/google'
+import '../styles/globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+
+const quicksand = Quicksand({ subsets: ['latin'] })
 
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
+    <AppProvider >
+      <div className={quicksand.className}>
+        <Component {...pageProps} />
+      </div>
     </AppProvider>
   )
 }
